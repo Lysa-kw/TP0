9c3044b17152773591170b133e8567a2c7ff5374
lysandra kingne welapenou <lysandrakingnewelapenou@MacBook-Air-de-lysandra.local>
2024-09-04 16:10:17 -0400

    Réponse à la question 1 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+A=(math.pi)*(rayon**2)
+print(f"l'aire du cercle de rayon {rayon} est {A}")
+print("kingne lysandra")
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
